b77471ada667640f68db41cda85f7ff4cd9ea151
ModuloM <modulom@modulom.net>
2015-02-17 09:04:00 +0100

    Selectable rank component

--- src/scripts/app.jsx
+++ src/scripts/app.jsx
@@ -3,13 +3,16 @@
 var React = require('react'),
     Rank = require('./modules/Rank.jsx');
 
-var data = [
-  {rank: 1},
-  {rank: 2},
-  {rank: 3},
-  {rank: 4},
+// var data = [
+//   {rank: 1},
+//   {rank: 2},
+//   {rank: 3},
+//   {rank: 4},
+//   {rank: 5}
+// ];
+var data = 
   {rank: 5}
-];
+;
 
 React.render(
     <Rank data={data} />,
--- src/scripts/modules/Rank.jsx
+++ src/scripts/modules/Rank.jsx
@@ -3,16 +3,45 @@
 var React = require('react');
 
 var Rank = React.createClass({
+  getInitialState: function() {
+    return {selected: false};
+  },
+  handleClick: function(event) {
+    this.setState({selected : !this.state.selected});
+  },
+  // render: function() {
+  //   var rankNodes = this.props.data.map(function (item){
+  //     var rankClass = 'rank--' + item.rank;
+  //     if (this[item.rank].state.selected) {
+  //       rankClass += ' tank--selected';
+  //     }
+      
+  //     return (
+  //       <div className="rank-container">
+  //         <div key={item.rank} className={rankClass} >
+  //           {item.rank}
+  //         </div>
+  //       </div>
+  //     );
+  //   });
+  //   return (
+  //     <div className="rank-list">
+  //       {rankNodes}
+  //     </div>
+  //   );
+  // }
   render: function() {
-    var rankNodes = this.props.data.map(function (item){
-      var rankClass = 'rank--' + item.rank;
-      return (
-        <div className={rankClass}>{item.rank}</div>
-      );
-    });
+    var ranking = this.props.data.rank;
+    var rankClass = 'rank--' + ranking;
+    if (this.state.selected) {
+      rankClass += ' rank--selected';
+    }
+
     return (
-      <div>
-        {rankNodes}
+      <div className="rank-container">
+        <div key={ranking} className={rankClass} onClick={this.handleClick} >
+          {ranking}
+        </div>
       </div>
     );
   }
